579c0a65dbaf50a9cbd2068bc5d88cca3ec4da0f
tsteelematc <tsteele@madisoncollege.edu>
2026-03-31 14:55:02 -0500

    Some initial placeholders...

--- src/PizzaToppings.tsx
+++ src/PizzaToppings.tsx
@@ -1,1 +1,36 @@
-export const PizzaToppings = () => <h2>Choose your pizza toppings...</h2>
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+export const PizzaToppings = () => {
+ 
+    //
+    // Return JSX...
+    // 
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title
+                    className="mb-3"
+                >
+                    Choose your pizza toppings...
+                </Card.Title>
+                <Button
+                    variant="outline-primary"
+                >
+                    All
+                </Button>
+                <Button
+                    className="ms-2"
+                    variant="outline-primary"
+                >
+                    None
+                </Button>
+                <h4
+                    className="mt-3"
+                >
+                    Total: $0.00
+                </h4>
+            </Card.Body>
+        </Card>
+    );
+}
