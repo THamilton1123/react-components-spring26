0cbd7a687fee12544cfcc927baa8da037b2dd936
THamilton1123 <thamilton12@madisoncollege.edu>
2026-04-16 12:54:25 -0500

    Update App.tsx
    Moved my component to the actual bottom

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import PizzaToppings from "./PizzaToppings";
 import TsteeleComponent from "./TsteeleComponent";
 import ThamiltonComponent from "./ThamiltonComponent";
+import Welcome from "./Welcome";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Welcome from "./Welcome";
 
 const App = () => {
   return (
@@ -13,8 +13,8 @@ const App = () => {
       <h1>React Components</h1>
       <PizzaToppings />
       <TsteeleComponent />
-      <ThamiltonComponent />
       <Welcome />
+      <ThamiltonComponent />
     </div>
   );
 };
