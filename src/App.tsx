72385c2198dd76250896773d4d7b26eebc98cb6f
THamilton1123 <thamilton12@madisoncollege.edu>
2026-04-20 12:16:06 -0500

    Moved my component to the top...

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Welcome from "./Welcome";
 import PizzaToppings from "./PizzaToppings";
 import TsteeleComponent from "./TsteeleComponent";
 import ThamiltonComponent from "./ThamiltonComponent";
-import DweiseComponent from './DweiseAccordionComponent';
-import CBaresComponent from './CBaresComponent';
-import ZsmuckerbryanComponent from './ZsmuckerbryanComponent';
-import MrooneyButton from './mrooneyButton';
-
+import DweiseComponent from "./DweiseAccordionComponent";
+import CBaresComponent from "./CBaresComponent";
+import ZsmuckerbryanComponent from "./ZsmuckerbryanComponent";
+import MrooneyButton from "./mrooneyButton";
 
 const App = () => {
   return (
     <div className="m-3">
       <h1>React Components</h1>
+      <ThamiltonComponent />
       <PizzaToppings />
       <TsteeleComponent />
       <Welcome />
-      <ThamiltonComponent />
       <DweiseComponent />
       <CBaresComponent />
       <ZsmuckerbryanComponent />
